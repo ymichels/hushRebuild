2f4f968bc6b284bb168e4756dc587e6eb9abe3ed
yehuda <yehuda.michelson@gmail.com>
2022-12-10 22:55:51 +0200

    api

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils.helper_functions import get_random_string
 #https://github.com/ymichels/hushRebuild
 
 
-def path_ORAM_test():
+def path_ORAM_test(number_of_blocks):
 
     # Results for this:
     # RAM.RT_WRITE:  10486081
@@ -18,8 +18,7 @@ def path_ORAM_test():
     # RAM.BALL_WRITE:  813189040
     # RAM.BALL_READ:  796917760
     # real_ram = {}
-    S = 23
-    path_oram = PathORAM(2**S,True)
+    path_oram = PathORAM(number_of_blocks,True)
     # allocating memory shouldn't count as 'writing'...
     reset_counters()
     # print(path_oram.access('write',0,b'1234'))
@@ -28,17 +27,17 @@ def path_ORAM_test():
     # print(path_oram.access('read',1))
     # print(path_oram.position_map_access(0))
     # print(path_oram.position_map_access(0))
-    for i in range(int((2**S)/1)):
+    for i in range(number_of_blocks):
         data = b'\x16'*path_oram.conf.BALL_DATA_SIZE
         # real_ram[i] = data
         path_oram.access('write',i,data)
         if i % 1_000 == 0:
             print(i,': ',len(path_oram.local_stash))
 
-    for i in range(2**(S)):
+    for i in range(number_of_blocks):
         if i % 1_000 == 0:
             print(i,': ',len(path_oram.local_stash))
-        key = random.randint(0,2**S - 1)
+        key = random.randint(0,number_of_blocks - 1)
         # if i < 2**S:
         #     oram_ans = path_oram.access('read', i)
         oram_ans = path_oram.access('read', key)
@@ -84,13 +83,13 @@ if False:
     
     
 #Debug test
-def debug_test():
+def debug_test(oram_size):
     # Results for this:
     # RAM.RT_WRITE:  5057017
     # RAM.RT_READ:  5056827
     # RAM.BALL_WRITE:  87572960
     # RAM.BALL_READ:  79399793
-    oram_size = 2**9*config.MU
+    # oram_size = 2**9*config.MU
     oram = ORAM(oram_size)
     oram.cleanWriteMemory()
 
@@ -112,23 +111,23 @@ def debug_test():
             j=0
         if i % 10_000 == 0:
             print('accesses: ',i)
-            print('not-found ratio: ', oram.not_found/(i+1))
-        
+            # print('not-found ratio: ', oram.not_found/(i+1))
+    
     print('RAM.RT_WRITE: ', local_RAM.RT_WRITE)
     print('RAM.RT_READ: ', local_RAM.RT_READ)
     print('RAM.BALL_WRITE: ', local_RAM.BALL_WRITE)
     print('RAM.BALL_READ: ', local_RAM.BALL_READ)
     print('done!')
 
-def ram_test():
-    size = int(((2**20)*100)/16)
-    ram = local_RAM('bla',config(size))
-    ram.generate_random_memory(size)
+def ram_test(number_of_blocks):
+    # size = int(((2**20)*100)/16)
+    ram = local_RAM('bla',config(number_of_blocks))
+    ram.generate_random_memory(number_of_blocks)
     reset_counters()
     print('generated')
-    for i in range(size*2):
-        ball = ram.readBall(random.randint(0,size-1)*ram.conf.BALL_SIZE)
-        ram.writeBall(random.randint(0,size-1)*ram.conf.BALL_SIZE,ball)
+    for i in range(number_of_blocks*2):
+        ball = ram.readBall(random.randint(0,number_of_blocks-1)*ram.conf.BALL_SIZE)
+        ram.writeBall(random.randint(0,number_of_blocks-1)*ram.conf.BALL_SIZE,ball)
         if i % 10_000 == 0:
             print('accesses: ',i)
     print('RAM.RT_WRITE: ', local_RAM.RT_WRITE)
@@ -143,10 +142,32 @@ def ram_test():
 import cProfile
 import pstats
 
+
+test_type = int(input('Enter test type:\n1) Our ORAM\n2) Path ORAM\n3) RAM\n'))
+number_of_MB = int(input('How many MB should the test run?\n'))
+number_of_blocks = int((number_of_MB*(2**20))/16)
 with cProfile.Profile() as pr:
-    ram_test()
+    if test_type == 1:
+        debug_test(number_of_blocks)
+    elif test_type == 2:
+        path_ORAM_test(number_of_blocks)
+    else:
+        ram_test(number_of_blocks)
 
 stats = pstats.Stats(pr)
 stats.sort_stats(pstats.SortKey.TIME)
-# stats.print_stats()
-stats.dump_stats(filename='ram_test_100MB.prof')
+stats.dump_stats(filename='test-{}.size-{}MB.prof'.format(test_type, number_of_MB))
+
+file = open('test-{}.size-{}MB'.format(test_type, number_of_MB),'w')
+file.write(
+    'accesses:{}\n'.format(number_of_blocks*2) +
+    'RAM.RT_WRITE:{}\n'.format(local_RAM.RT_WRITE) +
+    'RAM.RT_READ:{}\n'.format(local_RAM.RT_READ) +
+    'RAM.BALL_WRITE:{}\n'.format(local_RAM.BALL_WRITE) +
+    'RAM.BALL_READ:{}\n'.format(local_RAM.BALL_READ)
+)
+
+
+file.close()
+
+
