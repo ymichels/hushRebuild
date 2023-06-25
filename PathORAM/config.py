88f0b3e1204ea8a686af2c245e4e0fc61bc77983
Yehuda <yehuda.michelson@gmail.com>
2023-06-25 23:45:17 +0300

    commit

--- PathORAM/config.py
+++ PathORAM/config.py
@@ -4,16 +4,17 @@ import math
 from config import baseConfig
 
 
+BALL_DATA_SIZE = (2**20)
 class config(baseConfig):
 
 
     def __init__(self, N, is_map=False):
         self.N = N
         self.Z = 4
-        self.X = 4
+        self.X = int(BALL_DATA_SIZE/math.ceil(math.log(N,2)))
         self.KEY_SIZE = math.ceil(math.ceil(math.log(N,2))/8)
         if not is_map:
-            self.BALL_DATA_SIZE = 16
+            self.BALL_DATA_SIZE = BALL_DATA_SIZE
             # the balls structure:  KEY || LEAF || DATA
             self.BALL_SIZE = 2*self.KEY_SIZE + self.BALL_DATA_SIZE
         else:
@@ -23,7 +24,7 @@ class config(baseConfig):
             self.BALL_SIZE = 2*self.KEY_SIZE + self.BALL_DATA_SIZE
             # raise "not implemented yet"
         
-        self.LOCAL_MEMORY_SIZE = 3*2*30*(9**3)*16
+        self.LOCAL_MEMORY_SIZE = 131_220*self.BALL_DATA_SIZE#3*2*30*(9**3)*16
         self.LOCAL_MEMORY_SIZE_IN_BALLS = int(self.LOCAL_MEMORY_SIZE/self.BALL_SIZE)
         self.DATA_SIZE = N*self.BALL_SIZE*2*self.Z
         self.BUCKET_SIZE = self.Z*self.BALL_SIZE
